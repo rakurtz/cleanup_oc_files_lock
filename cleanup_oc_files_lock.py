2f5b18ff80091d8a714ddb9935247facea10f03b
rakurtz <kurtz@ra-kurtz.de>
2024-05-17 23:50:32 +0200

    added the functionality to notify about affected user_ids

--- cleanup_oc_files_lock.py
+++ cleanup_oc_files_lock.py
@@ -37,6 +37,17 @@ time_threshold = datetime.now() - timedelta(hours=time_delta_in_hours)
 conn = mysql.connector.connect(**db_config)
 cursor = conn.cursor()
 
+# Get user_id (these might need to update their sync-clients)
+select_query = """
+SELECT unique user_id FROM oc_files_lock 
+WHERE creation < %s
+"""
+cursor.execute(select_query, (time_threshold,))
+rows = cursor.fetchall() # returns a list of tuples like (user_id, some_other_value)
+
+user_ids = "\n".join([t[0] for t in rows]) # joins the first element of the above tuple to a newline separated string
+
+
 # Delete records older than 24 hours
 delete_query = """
 DELETE FROM oc_files_lock 
@@ -52,9 +63,12 @@ cursor.close()
 conn.close()
 
 # print result and notify via mail if result > 0
-result_message = f"{sys.argv[0]}: {cursor.rowcount} rows from oc_files_lock older than 24h deleted."
+result_message = f"{sys.argv[0]}: {cursor.rowcount} rows from oc_files_lock older than {time_delta_in_hours}h deleted."
 print(result_message)
 
+result_message = f"{result_message}\n\n user_ids:\n {user_ids}"
 if affected_rows > 0:
-    mail_command = f"echo {result_message} | mail -s 'nc_files_lock: Deleted values older than {time_delta_in_hours}h' {notify_to_email}"
+    mail_command = f"""mail -s 'nc_files_lock: Deleted values older than {time_delta_in_hours}h' {notify_to_email} << EOF
+{result_message}
+EOF"""
     os.system(mail_command)
